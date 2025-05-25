cc19d3c0954c12b05cd11e33877a2f4085e93087
Alex9600t <dukovaleks2@gmail.com>
2025-05-25 08:13:59 +0300

    fix(I'm dolbaeb)

--- src/modules/neofetch.c
+++ src/modules/neofetch.c
@@ -11,7 +11,7 @@ void neofetch() {
     color0();
     print("    /\\          "); color1(); print("OS: GovnoOS\n"); color0();
     print("   ( /)         "); color1(); print("Kernel: GovnoKernel\n"); color0();
-    print("  (/  /)        "); color1(); print("CPU: i386\n"); color0();
-    print(" (___/__)       "); color1(); print("RAM: 512MB\n"); color0(); //Да, я не умею юзать %d
-    print("(___/___/)      "); color1(); print("Resolution: "); print(int_to_char(VGA_TERMINAL_WIDTH)); print("x"); print(int_to_char(VGA_TERMINAL_HEIGHT)); print("\n"); color0();
+    print("  (/  /)        "); color1(); print("CPU: i686\n"); color0();
+    print(" (__/__/)       "); color1(); print("RAM: 512MB\n"); color0(); //Да, я не умею юзать %d
+    print("(_/__/__/)      "); color1(); print("Resolution: "); print(int_to_char(VGA_TERMINAL_WIDTH)); print("x"); print(int_to_char(VGA_TERMINAL_HEIGHT)); print("\n"); color0();
 }
