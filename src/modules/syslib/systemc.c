cab50dcdd6cf2be451728e1f60cce79b46735c1b
L1stak <113133819+L1stak@users.noreply.github.com>
2025-05-28 19:03:35 +0000

    keyboardDriver

--- src/modules/syslib/systemc.c
+++ src/modules/syslib/systemc.c
@@ -193,3 +193,34 @@ static uint64_t rdtsc() // считывает текущее значение с
     asm volatile ( "rdtsc" : "=A"(ret) );
     return ret;
 }
+#include <stdint.h>
+
+void* memset(void* dest, int val, uint32_t len)
+{
+    unsigned char* ptr = dest;
+    unsigned char vval = val;
+    while (len-- > 0)
+        *ptr++ = vval;
+    return dest;
+}
+
+void* memmove (void *dest, const void *src, uint32_t len)
+{
+    char *d = dest;
+    const char *s = src;
+    
+    if (d < s)
+    {
+        while (len--)
+            *d++ = *s++;
+    }
+    else
+    {
+        const char* lasts = s + (len-1);
+        char* lastd = d + (len-1);
+        while (len--)
+            *lastd-- = *lasts--;
+    }
+
+    return dest;
+}
