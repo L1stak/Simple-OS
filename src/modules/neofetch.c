097f9a6433a21a359d30e81bbd14b3ccca608919
L1stak <113133819+L1stak@users.noreply.github.com>
2025-05-28 18:30:20 +0000

    feat(syslib) added hash table and keyboard driver base

--- src/modules/neofetch.c
+++ src/modules/neofetch.c
@@ -7,9 +7,10 @@ void color1() {
     terminalSetColor(VGA_COLOR_DARK_GREY, VGA_COLOR_BLACK);
 }
 
+
 void neofetch() {
     color0();
-    print("    /\\          "); color1(); print("OS: GovnoOS\n"); color0();
+    print("    /\\          "); color1(); print("OS: GovnoOS\n"); color0();  
     print("   ( /)         "); color1(); print("Kernel: GovnoKernel\n"); color0();
     print("  (/  /)        "); color1(); print("CPU: i686\n"); color0();
     print(" (__/__/)       "); color1(); print("RAM: 512MB\n"); color0(); //Да, я не умею юзать %d
