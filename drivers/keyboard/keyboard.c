24b1744de6f24c7709dcfe146b14d1f8689e483c
L1stak <maxbel337@gmail.com>
2025-06-01 21:55:23 +0300

    feat(drivers) added keyboard driver base

---
+++ drivers/keyboard/keyboard.c
@@ -0,0 +1,41 @@
+#include "keyboard.h"
+#include "../std.h"
+
+
+static char* _qwertzuiop = "qwertzuiop"; // 0x10-0x1c
+static char* _asdfghjkl = "asdfghjkl";
+static char* _yxcvbnm = "yxcvbnm";
+static char* _num = "123456789";
+uint8_t keyboard_to_ascii(uint8_t key)
+{
+	
+	if(key == 0x1C) return '\n';
+	if(key == 0x39) return ' ';
+	if(key == 0xE) return '\r';
+	if(key == POINT_RELEASED) return '.';
+	if(key == SLASH_RELEASED) return '/';
+	if(key == ZERO_PRESSED) return '0';
+	if(key >= ONE_PRESSED && key <= NINE_PRESSED)
+		return _num[key - ONE_PRESSED];
+	if(key >= 0x10 && key <= 0x1C)
+	{
+		return _qwertzuiop[key - 0x10];
+	} else if(key >= 0x1E && key <= 0x26)
+	{
+		return _asdfghjkl[key - 0x1E];
+	} else if(key >= 0x2C && key <= 0x32)
+	{
+		return _yxcvbnm[key - 0x2C];
+	}
+	return 0;
+}
+
+void keyboard_IRQ(uint16_t code){
+
+terminalClear(VGA_COLOR_BLACK);
+terminalSetColor(VGA_COLOR_GREEN,VGA_COLOR_BLACK);
+print("KEYBOARD BUTTON PRESSED");
+
+
+
+}
