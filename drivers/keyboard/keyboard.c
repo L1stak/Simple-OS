cf5bfaf756fc256f6e0f8203eebccabcd9f6fc77
L1stak <113133819+L1stak@users.noreply.github.com>
2025-06-05 22:58:05 +0300

    Update keyboard.c

--- drivers/keyboard/keyboard.c
+++ drivers/keyboard/keyboard.c
@@ -1,4 +1,4 @@
-#   Copyright (C) 2025 L1stak
+/*  Copyright (C) 2025 L1stak
 #
 # This file is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-
+*/
 #include "keyboard.h"
 #include "../../modules/std.h"
 
